73d5b2223040ae87075bd5e385b5c27bab929b19
Guillaume Maïssa <guillaume@maissa.fr>
2017-04-14 22:19:04 +0200

    fix: update default entity mapping

---
+++ src/Entity/RedmineUser.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * File part of the Redmine User Provider bundle
+ *
+ * @category  SymfonyBundle
+ * @package   GMaissa.RedmineUserProviderBundle
+ * @author    Guillaume Maïssa <pro.g@maissa.fr>
+ * @copyright 2017 Guillaume Maïssa
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace GMaissa\RedmineUserProviderBundle\Entity;
+
+use GMaissa\RedmineUserProviderBundle\Model\RedmineUser as BaseRedmineUser;
+
+/**
+ * User Entity Class
+ */
+class RedmineUser extends BaseRedmineUser
+{
+}
